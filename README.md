afd514a44af4c0769f16776931b5f03621a10798
Adam Smith <adam.smith@digital.homeoffice.gov.uk>
2020-02-12 21:38:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## go-clamav-rest
+
+Clamav proxy based on https://github.com/solita/clamav-rest/ and implemented in golang
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/asmith030/go-clamav-rest/server"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	host       = flag.String("host", "localhost", "Address of the clamd instance")
+	port       = flag.Int("port", 3310, "TCP port of the clamd instance")
+	listenPort = flag.Int("listenPort", 8080, "TCP port that we should listen on")
+)
+
+func newLogger() *logrus.Logger {
+	var logger = logrus.New()
+	logger.Out = os.Stderr
+	jsonFormatter := new(logrus.JSONFormatter)
+	jsonFormatter.TimestampFormat = time.RFC3339Nano
+	logger.Formatter = jsonFormatter
+	logger.Level = logrus.InfoLevel
+	return logger
+}
+
+func main() {
+	flag.Parse()
+	logger := newLogger()
+
+	server.RunHTTPListener(
+		fmt.Sprintf("tcp://%v:%d", *host, *port),
+		*listenPort, logger)
+}
